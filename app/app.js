0f6e54264d4503f0aeb6807f1decda3d3414c83b
Steve Haran <sharan@homeaway.co.uk>
2015-05-22 17:23:30 +0100

    initial checkin

---
+++ app/app.js
@@ -0,0 +1,33 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+  '$scope',
+  'posts',
+  function($scope, posts){
+    $scope.test = 'Hello world!';
+
+    $scope.posts = posts.posts;
+
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+
+  }]);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
